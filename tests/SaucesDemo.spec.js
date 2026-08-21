5182681fca919bf470ecaf4ed2561a0adc339829
Kajalsthorat27 <156704179+Kajalsthorat27@users.noreply.github.com>
2026-08-21 15:17:39 +0530

    Update SaucesDemo.spec.js

--- tests/SaucesDemo.spec.js
+++ tests/SaucesDemo.spec.js
@@ -23,8 +23,8 @@ test("@Regression End To End Flow", async ({ page }) =>{
     await page.waitForTimeout(7000);
 
     // used to locate Single element in HTML DOM Structure
-    const container = await page.$("//span[@class='shopping_cart_badge']");
-    await container.click();
+    //const container = await page.$("//span[@class='shopping_cart_badge']");
+   // await container.click();
 
-    await page.waitForTimeout(5000);
+   // await page.waitForTimeout(5000);
 });
