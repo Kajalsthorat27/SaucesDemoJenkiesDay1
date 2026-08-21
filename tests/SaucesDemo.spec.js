1c31d317f49e0cf7b949a69de47d9d7824faddc0
Kajal Thorat <Kajalsthorat27@gmail.com>
2026-08-21 11:52:13 +0530

    updated code

--- tests/SaucesDemo.spec.js
+++ tests/SaucesDemo.spec.js
@@ -1,20 +1,22 @@
-import { test , chromium } from '@playwright/test';
+import { test, chromium } from '@playwright/test';
 
-test(" @Regrasstion End to End Flows", async ()=>{
+test("@Regression End To End Flow", async () =>{
+    const browser = await chromium.launch({headless : false});
 
-    const browser =await chromium.launch({headless:false});
-    const page  = await browser.newPage();
+    const page = await browser.newPage();
 
     await page.goto("https://www.saucedemo.com/");
 
     await page.locator("#user-name").fill("standard_user");
     await page.locator("#password").fill("secret_sauce");
 
+    await page.waitForTimeout(3000);
+
     await page.locator("#login-button").click();
 
     await page.waitForTimeout(7000);
 
-   await page.locator(".product_sort_container").selectOption({label : 'Price (high to low)'});
+    await page.locator(".product_sort_container").selectOption({label : 'Price (high to low)'});
 
     await page.waitForTimeout(3000);
 
@@ -22,7 +24,9 @@ test(" @Regrasstion End to End Flows", async ()=>{
 
     await page.waitForTimeout(3000);
 
+    // used to locate Single element in HTML DOM Structure
+    const container = await page.$("//span[@class='shopping_cart_badge']");
+    await container.click();
 
-
-
+    await page.waitForTimeout(3000);
 });
