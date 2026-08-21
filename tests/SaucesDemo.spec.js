ddce90cf3f567651dead581535b4540d1a834e61
Kajal Thorat <Kajalsthorat27@gmail.com>
2026-08-21 14:38:54 +0530

    Update Playwright config file

--- tests/SaucesDemo.spec.js
+++ tests/SaucesDemo.spec.js
@@ -1,16 +1,14 @@
-import { test, chromium } from '@playwright/test';
+import { test } from '@playwright/test';
 
-test("@Regression End To End Flow", async () =>{
-    const browser = await chromium.launch({headless : false});
-
-    const page = await browser.newPage();
+test("@Regression End To End Flow", async ({ page }) =>{
+   
 
     await page.goto("https://www.saucedemo.com/");
 
     await page.locator("#user-name").fill("standard_user");
     await page.locator("#password").fill("secret_sauce");
 
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(5000);
 
     await page.locator("#login-button").click();
 
@@ -18,15 +16,15 @@ test("@Regression End To End Flow", async () =>{
 
     await page.locator(".product_sort_container").selectOption({label : 'Price (high to low)'});
 
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(5000);
 
     await page.locator("button[name='add-to-cart-sauce-labs-fleece-jacket']").click();
 
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(7000);
 
     // used to locate Single element in HTML DOM Structure
     const container = await page.$("//span[@class='shopping_cart_badge']");
     await container.click();
 
-    await page.waitForTimeout(3000);
+    await page.waitForTimeout(5000);
 });
